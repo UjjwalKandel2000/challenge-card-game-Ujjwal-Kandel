297b44196230802c21f47127e3993117f6615b93
UjjwalKandel2000 <ujjwalkandel2000@gmail.com>
2022-01-23 11:47:43 +0100

    updated card class

--- utils/card.py
+++ utils/card.py
@@ -1,24 +1,49 @@
+import streamlit as st
+
 class Symbol:
     """Class defining a symbol based on colors and icons,"""
-
+  
     def __init__(self,icon):
-        """By default, our surface is empty"""
-        self.color = ["red", "black"]
+        """By default, our color and icon is empty"""   
+        self.color = ""
         self.icon = icon
+    
 
     def __str__(self):
         return f" {self.icon}"
+    
 
 
 class Card(Symbol):
     def __init__(self,icon,value):
         super(Card,self).__init__(icon)
         self.value = value
+        self.set_color()
 
     def show(self):
         print(f" {self.value} {super().__str__()}")
+        st.write(f" {self.value} {super().__str__()}")
+        
 
     def __str__(self):
-        return f"{self.value} {super().__str__()} "
+        return f"{self.value} {super().__str__()}"
 
+    def get_color(self):
+        return self.color
+
+    def set_color(self):
+        if self.icon=="♥":
+            self.color = "red"
+        elif self.icon=="♦":
+            self.color = "red"
+        else: self.color = "black"
+        
+    def get_color(self):
+        return self.color
+
+    def get_icon(self):
+        return self.icon
+    def get_value(self):
+        return self.value
+    
 
