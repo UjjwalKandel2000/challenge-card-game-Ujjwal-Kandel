182f4a66f80252e8c421fad1fa743fa086f3aec6
UjjwalKandel2000 <ujjwalkandel2000@gmail.com>
2022-01-23 10:56:18 +0100

    made a streamlit webpage to deploy my project

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import streamlit as st
 from utils.player import Deck
 from utils.player import Player
 from utils.game import Board
 
 deck1 = Deck()
 
-p1 = Player("ek", deck1)
-p2 = Player("dui", deck1)
-p3 = Player("tin", deck1)
-p4 = Player("chhar", deck1)
+player_name1 = st.text_input(f"name of player 1")
+player_name2 = st.text_input(f"name of player 2")
+player_name3 = st.text_input(f"name of player 3")
+player_name4 = st.text_input(f"name of player 4")
 
-group = [p1, p2, p3, p4]
+if player_name4:
+    p1 = Player(player_name1, deck1)
+    p2 = Player(player_name2, deck1)
+    p3 = Player(player_name3, deck1)
+    p4 = Player(player_name4, deck1)
 
-board = Board(group)
-board.start_game()
+    group = [p1, p2, p3, p4]
+
+    board = Board(group)
+    board.start_game()
